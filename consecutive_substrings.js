f3198bd55721a0085ba9e4d1f6d2415481a7d9c6
Porter <pdhakes25@gmail.com>
2025-12-16 12:56:12 -0700

    working function

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,6 +1,20 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const individual = [...string]
+  const combined = []
+  const length = individual.length
+  
+  for(let i = 0; i < length; i++) {
+    for(let j = (i + 1); j <= length; j++) {
+      const substring = individual.slice(i,j).join('')
+      combined.push(substring)
+    }
+
+  }
+
+  return combined
 }
+
+//Big O(n^2)
 
 if (require.main === module) {
   // add your own tests in here
